4920aaef2f90cf985d98269f09f782ba852ff8ee
Joe Rogers <rogersj17@gmail.com>
2017-09-26 13:57:14 -0400

    Moved the order of some tests.

--- RentACatTest.java
+++ RentACatTest.java
@@ -9,10 +9,6 @@ public class RentACatTest
   RentACat _rac;
 
 
-  /*PLEASE NOTE: These tests do NOT use mocks - I found it appropriate to test them in a way i knew first and left these tests in.
-    These include testing empty lists or nonexistent cats. 
-   Tests with mocks are below these at line 103.*/
-
   @Before
   public void setup()
   {
@@ -23,6 +19,68 @@ public class RentACatTest
   }
 
 
+    //Tests if a mock cat exists within a list using a stub method for getId().
+    @Test
+    public void mockCatExists()
+    {
+      RentACat test = new RentACat();
+      Cat c = Mockito.mock(Cat.class);
+      Mockito.when(c.getId()).thenReturn(4);
+      test._cats.add(c);
+      assertTrue(_rac.catExists(4, test._cats));
+    }
+
+    //Tests if you can rent a mock cat. Cat is available by default.
+    @Test
+    public void rentMockCat()
+    {
+      RentACat test = new RentACat();
+      Cat c = Mockito.mock(Cat.class);
+      test._cats.add(c);
+      assertTrue(test.rentCat(c));
+    }
+
+    //Tests if you can return an available mock cat. Test should return false because you cannot return a cat that is still available.
+    @Test
+    public void returnAvailableMockCat()
+    {
+      RentACat test = new RentACat();
+      Cat c = Mockito.mock(Cat.class);
+      test._cats.add(c);
+      assertFalse(test.returnCat(c));
+    }
+
+    //Tests if you can return a list of mock cats. Uses a stub method for toString() to control what string is returned.
+    @Test
+    public void returnListOfMockCats()
+    {
+      RentACat test = new RentACat();
+      Cat c = Mockito.mock(Cat.class);
+      Mockito.when(c.toString()).thenReturn("1. Gerald");
+      Cat c2 = Mockito.mock(Cat.class);
+      Mockito.when(c2.toString()).thenReturn("2. Geraldine");
+      test._cats.add(c);
+      test._cats.add(c2);
+      assertEquals("1. Gerald\n2. Geraldine\n", test.listCats(test._cats));
+    }
+
+    //Tests if you can return a mock cat that has been rented.
+    @Test
+    public void returnMockCatSuccessfully()
+    {
+      RentACat test = new RentACat();
+
+      Cat c = new Cat(1, "Geraldine");
+      Cat cat = Mockito.spy(c);
+      test._cats.add(cat);
+      test.rentCat(cat);
+      assertTrue(test.returnCat(cat));
+    }
+
+
+/*
+  PLEASE NOTE: Tests below do not use Mocks, but I wrote these to originally test my code. They include testing empty lists.  
+*/
 
   //Test to see if returning an available cat returns false.
   @Test
@@ -101,65 +159,7 @@ public class RentACatTest
   }
 
 
-  /*BEGIN TESTS WITH MOCKS AND STUBS*/
 
-  //Tests if a mock cat exists within a list using a stub method for getId().
-  @Test
-  public void mockCatExists()
-  {
-    RentACat test = new RentACat();
-    Cat c = Mockito.mock(Cat.class);
-    Mockito.when(c.getId()).thenReturn(4);
-    test._cats.add(c);
-    assertTrue(_rac.catExists(4, test._cats));
-  }
-
-  //Tests if you can rent a mock cat. Cat is available by default.
-  @Test
-  public void rentMockCat()
-  {
-    RentACat test = new RentACat();
-    Cat c = Mockito.mock(Cat.class);
-    test._cats.add(c);
-    assertTrue(test.rentCat(c));
-  }
-
-  //Tests if you can return an available mock cat. Test should return false because you cannot return a cat that is still available.
-  @Test
-  public void returnAvailableMockCat()
-  {
-    RentACat test = new RentACat();
-    Cat c = Mockito.mock(Cat.class);
-    test._cats.add(c);
-    assertFalse(test.returnCat(c));
-  }
-
-  //Tests if you can return a list of mock cats. Uses a stub method for toString() to control what string is returned.
-  @Test
-  public void returnListOfMockCats()
-  {
-    RentACat test = new RentACat();
-    Cat c = Mockito.mock(Cat.class);
-    Mockito.when(c.toString()).thenReturn("1. Gerald");
-    Cat c2 = Mockito.mock(Cat.class);
-    Mockito.when(c2.toString()).thenReturn("2. Geraldine");
-    test._cats.add(c);
-    test._cats.add(c2);
-    assertEquals("1. Gerald\n2. Geraldine\n", test.listCats(test._cats));
-  }
-
-  //Tests if you can return a mock cat that has been rented.
-  @Test
-  public void returnMockCatSuccessfully()
-  {
-    RentACat test = new RentACat();
-
-    Cat c = new Cat(1, "Geraldine");
-    Cat cat = Mockito.spy(c);
-    test._cats.add(cat);
-    test.rentCat(cat);
-    assertTrue(test.returnCat(cat));
-  }
 
 
 }
